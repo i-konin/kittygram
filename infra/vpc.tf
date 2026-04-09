0a930d02e7271550706fee8a9e637f72d3dd1b9b
Ilya Konin <i.konin.88@yandex.ru>
2026-04-09 22:54:56 +0300

    fix

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -6,7 +6,7 @@ resource "yandex_vpc_network" "network_1" {
 # Создаем подсеть ВНУТРИ этой сети
 resource "yandex_vpc_subnet" "subnet_1" {
   name           = "kittygram-subnet"
-  zone           = var.aws_region # Убедись, что регион тот же, что у ВМ
+  zone           = var.zone
   network_id     = yandex_vpc_network.network_1.id
-  v4_cidr_blocks = ["10.129.1.0/24"] # Любой свободный диапазон
+  v4_cidr_blocks = ["10.129.1.0/24"]
 }
