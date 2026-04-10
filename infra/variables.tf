6eea99423877ada2989054b527ee56b0297472ce
Ilya Konin <i.konin.88@yandex.ru>
2026-04-10 09:24:29 +0300

    last fix

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,12 +2,6 @@
 # Variables
 # ===============
 
-variable "vpc_name" {
-  description = "VPC Name"
-  type        = string
-  default     = "infra-network"
-}
-
 variable "net_cidr" {
   description = "Subnet structure"
   type = list(object({
@@ -17,21 +11,8 @@ variable "net_cidr" {
   }))
 
   default = [
-    { name = "infra-subnet-a", zone = "ru-central1-a", prefix = "10.129.1.0/24" },
-    { name = "infra-subnet-b", zone = "ru-central1-b", prefix = "10.130.1.0/24" },
-    { name = "infra-subnet-d", zone = "ru-central1-d", prefix = "10.131.1.0/24" },
+    { name = "infra-subnet-a", zone = "ru-central1-a", prefix = "10.129.1.0/24" }
   ]
-}
-
-variable "vm_1_name" {
-  type        = string
-  default     = "vm-kittygram"
-}
-
-variable "aws_region" {
-  description = "AWS Region name"
-  type        = string
-  default     = "ru-central1"
 }
 
 variable "ssh_key" {
@@ -55,18 +36,6 @@ variable "zone" {
   default     = "ru-central1-a"
 }
 
-variable "platform_id" {
-  description = "Платформа ВМ"
-  type        = string
-  default     = "standard-v2"
-}
-
-variable "image_family" {
-  description = "Семейство образа ОС"
-  type        = string
-  default     = "ubuntu-2004-lts"
-}
-
 variable "cores" {
   description = "Количество ядер CPU"
   type        = number
@@ -83,12 +52,6 @@ variable "disk_type" {
   description = "Тип диска (network-hdd, network-ssd)"
   type        = string
   default     = "network-ssd"
-}
-
-variable "disk_size" {
-  description = "Размер диска (ГБ)"
-  type        = number
-  default     = 40
 }
 
 variable "nat" {
